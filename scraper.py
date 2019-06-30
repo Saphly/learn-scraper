2c73d6685c1f25a9d68a84ab21bf3ccb0026b7be
Saphly <kee.wei26@gmail.com>
2019-06-30 21:35:43 +0100

    Managed to login to learn

--- scraper.py
+++ scraper.py
@@ -1,7 +1,12 @@
-def hello_world():
-    print('Hello World')
-
-def main():
-    hello_world()
-
-main()
+import requests
+import config
+with requests.Session() as s:
+    res = s.get('https://www.ease.ed.ac.uk/cosign.cgi')
+    cookies = dict(res.cookies)
+    loginfo = {'login': config.login, 
+               'password': config.password,
+               'ref': 'https://www.learn.ed.ac.uk/cgi-bin/login.cgi',
+               'service': 'cosign-eucsCosign-www.learn.ed.ac.uk'}
+    r = requests.post('https://www.ease.ed.ac.uk/cosign.cgi', data=loginfo, cookies=cookies)
+    print(r.status_code)
+    print(r.text)
