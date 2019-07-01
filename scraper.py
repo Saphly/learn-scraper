a45eaaa53621fdf4fc8a73c70641f452d8897a8c
Saphly <kee.wei26@gmail.com>
2019-07-01 23:24:29 +0100

    Managed to get into Learn and download contents from first page

--- scraper.py
+++ scraper.py
@@ -5,25 +5,50 @@ import re
 from bs4 import BeautifulSoup
 
 COURSE_NAME = 'Linear Programming, Modelling and Solution'
+LEARN_URL = 'https://www.learn.ed.ac.uk'
 
 with requests.Session() as s:
     res = s.get('https://www.ease.ed.ac.uk/cosign.cgi')
     cookies = dict(res.cookies)
     loginfo = {'login': config.login, 
                'password': config.password,
-               'ref': 'https://www.learn.ed.ac.uk/cgi-bin/login.cgi',
+               'ref': LEARN_URL + '/cgi-bin/login.cgi',
                'service': 'cosign-eucsCosign-www.learn.ed.ac.uk'}
     r = s.post('https://www.ease.ed.ac.uk/cosign.cgi', data=loginfo, cookies=cookies)
-    
+
     data = {'action': 'refreshAjaxModule', 
                'modId': '_4_1',
                'tabId': '_1_1',
                'tab_tab_group_id': '_171_1'}
-    r = s.post('https://www.learn.ed.ac.uk/webapps/portal/execute/tabs/tabAction', data=data)
-    html = r.text
-    soup = BeautifulSoup(html, 'html.parser')
+    r = s.post(LEARN_URL + '/webapps/portal/execute/tabs/tabAction', data=data)
+    soup = BeautifulSoup(r.text, 'lxml')
 
-    print(html)
+    course_link = soup.find('a', string=re.compile(COURSE_NAME + '.*')).get('href').strip()
+    course_id = re.compile('(?:\/webapps\/blackboard\/execute\/launcher\?type=Course&id=)(.*)&(?:.*)').search(course_link).group(1)
+    r = s.get(LEARN_URL + course_link)
+    course_url = r.url
+    soup = BeautifulSoup(r.text, 'lxml')
 
-    # for link in soup.find_all('a'):
-    #     print(link)#print(soup.find('a', href='/webapps/blackboard/execute/launcher?type=Course&id=_65022_1&url='))
+    
+    content_links = soup.find_all('a', href=re.compile('\/bbcswebdav\/.*'))
+    for content_link in content_links:
+        r = s.get(LEARN_URL + content_link.get('href'))
+        content_name = r.url.split('/')[-1]
+        with open(content_name, 'wb') as f:
+            f.write(r.content)
+
+    # folders
+    dir_links = soup.find_all('a', href=re.compile('\/webapps\/blackboard\/content\/listContent\.jsp\?course_id={}&content_id=.*'.format(course_id)))
+    for dir_link in dir_links:  
+        if course_url not in LEARN_URL + dir_link.get('href'):
+            print(dir_link.get('href'))
+            r = s.get(LEARN_URL + dir_link.get('href'))
+            soup = BeautifulSoup(r.text, 'lxml')
+            content_links = soup.find_all('a', href=re.compile('\/bbcswebdav\/.*'))
+            print('{}: \n {} \n\n'.format(dir_link, content_links))
+            for content_link in content_links:
+                r = s.get(LEARN_URL + content_link.get('href'))
+                content_name = r.url.split('/')[-1]
+                with open(content_name, 'wb') as f:
+                    f.write(r.content)
+
